25f23ee03a85a313944500b818f259d219b3b4a3
Gerardo Hidalgo-Cuellar <ghidalgo93@gmail.com>
2021-01-12 16:28:58 -0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,55 @@
+import "./style.css";
+
+const form = document.getElementById("citySearch");
+const div = document.createElement("div");
+document.body.appendChild(div);
+
+async function getWeather(city, units) {
+  try {
+    const apiKey = "af7be5386c68edc8a11bfc3e58d4d6ac";
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${apiKey}`;
+    const response = await fetch(url, { mode: "cors" });
+    const weatherData = await response.json();
+    div.textContent = processWeatherJson(weatherData).city;
+  } catch (err) {
+    throw Error(`city not found, ${err}`);
+  }
+}
+
+function processWeatherJson(data) {
+  try {
+    const city = data.name;
+    const { country } = data.sys;
+    const weather = data.weather[0].main;
+    const { description } = data.weather[0];
+    const { temp } = data.main;
+    const feelsLike = data.main.feels_like;
+    const high = data.main.temp_max;
+    const low = data.main.temp_min;
+    const { sunrise } = data.sys;
+    const { sunset } = data.sys;
+
+    return {
+      city,
+      country,
+      weather,
+      description,
+      temp,
+      feelsLike,
+      high,
+      low,
+      sunrise,
+      sunset,
+    };
+  } catch (err) {
+    throw Error("data undefined");
+  }
+}
+
+form.onsubmit = (e) => {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const plainFormData = Object.fromEntries(formData.entries());
+  getWeather(plainFormData.city, "imperial");
+  e.target.reset();
+};
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: ["@babel/polyfill", "./src/index.js"],
+  devtool: "source-map",
+  devServer: {
+    contentBase: "./dist",
+  },
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
